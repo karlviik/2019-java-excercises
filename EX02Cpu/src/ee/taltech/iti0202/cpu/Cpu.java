4c65f892a926060cb650268176c8ddc2cc789d66
karlviik <viik.karl2@gmail.com>
2019-02-07 13:46:12 +0200

    Initial solution

--- EX02Cpu/src/ee/taltech/iti0202/cpu/Cpu.java
+++ EX02Cpu/src/ee/taltech/iti0202/cpu/Cpu.java
@@ -4,6 +4,13 @@ import java.util.Map;
 
 public class Cpu {
 
+    private static final int CHANGED_VAR = 0;
+    private static final int INC_DEC = 1;
+    private static final int CHANGE_VALUE = 2;
+    private static final int COND_VAR = 4;
+    private static final int COMP_TYPE = 5;
+    private static final int COMP_VALUE = 6;
+
     private static boolean checkIf(HashMap<String, Integer> registers, String var, String type, int value) {
         int varValue = registers.get(var);
         switch (type) {
@@ -28,11 +35,12 @@ public class Cpu {
         HashMap<String, Integer> registers = new HashMap<>();
         for(String line : instructions.split("\n")) {
             String[] taskComponents = line.split(" ");
-            int incOrDec = taskComponents[1].equals("inc") ? 1 : -1;
-            registers.put(taskComponents[4], registers.getOrDefault(taskComponents[4], 0));
-            registers.put(taskComponents[0], registers.getOrDefault(taskComponents[0], 0)
-                    + (checkIf(registers, taskComponents[4], taskComponents[5], Integer.parseInt(taskComponents[6]))
-                    ? 1 : 0) * incOrDec * Integer.parseInt(taskComponents[2]));
+            int incOrDec = taskComponents[INC_DEC].equals("inc") ? 1 : -1;
+            registers.put(taskComponents[COND_VAR], registers.getOrDefault(taskComponents[COND_VAR], 0));
+            registers.put(taskComponents[CHANGED_VAR], registers.getOrDefault(taskComponents[CHANGED_VAR],
+                    0) + (checkIf(registers, taskComponents[COND_VAR], taskComponents[COMP_TYPE],
+                    Integer.parseInt(taskComponents[COMP_VALUE])) ? 1 : 0) * incOrDec
+                    * Integer.parseInt(taskComponents[CHANGE_VALUE]));
         }
         return registers;
     }
