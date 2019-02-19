12094911b8388771cd5053b40bbb10f8d862b7d4
karlviik <viik.karl2@gmail.com>
2019-02-19 22:45:47 +0200

    PR04 initial structure sync

---
+++ PR04Stock/src/ee/taltech/iti0202/stock/product/Product.java
@@ -0,0 +1,24 @@
+package ee.taltech.iti0202.stock.product;
+import ee.taltech.iti0202.stock.exceptions.StockException;
+
+public class Product {
+
+    public Product(String name, int price) throws StockException {
+    }
+
+    public static int getNextId() {
+        return 0;
+    }
+
+    public int getId() {
+        return 0;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public int getPrice() {
+        return 0;
+    }
+}
