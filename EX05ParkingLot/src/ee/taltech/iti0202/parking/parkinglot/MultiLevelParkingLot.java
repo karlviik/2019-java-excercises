e88f2d86c686babd7ff4217d3a95bd48ed34e663
karlviik <viik.karl2@gmail.com>
2019-03-10 18:37:27 +0200

    MultiLevel initial

--- EX05ParkingLot/src/ee/taltech/iti0202/parking/parkinglot/MultiLevelParkingLot.java
+++ EX05ParkingLot/src/ee/taltech/iti0202/parking/parkinglot/MultiLevelParkingLot.java
@@ -3,6 +3,10 @@ package ee.taltech.iti0202.parking.parkinglot;
 
 import ee.taltech.iti0202.parking.car.Car;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * Modern parking lot located under ground.
  * The parking lot has several levels.
@@ -15,6 +19,13 @@ import ee.taltech.iti0202.parking.car.Car;
  *
  */
 public class MultiLevelParkingLot extends ParkingLot {
+
+    private final int levels;
+    private Car[][][][] map;
+    private HashMap<Car, ArrayList<Integer[]>> locations = new HashMap<>();
+    // queue
+    // parkedcars
+
     /**
      * Initialize the parking slot with the given width and height.
      *
@@ -24,11 +35,67 @@ public class MultiLevelParkingLot extends ParkingLot {
      */
     public MultiLevelParkingLot(int height, int width, int levels) {
         super(height, width);
+        this.levels = levels;
+        map = new Car[levels][height][width][2];
     }
 
     @Override
     public void processQueue() {
+        while (queue.size() > 0) {
+            Car car = queue.peek();
+            ArrayList<Integer[]> coords = getParkingSpot(car.getSize());
+            if (coords.size() == 0) {
+                return;
+            }
+            parkedCars.add(queue.poll());
+            locations.put(car, coords);
+            car.setParked();
+            for (Integer[] coord : coords) {
+                map[coord[0]][coord[1]][coord[2]][coord[3]] = car;
+            }
 
+        }
+    }
+
+    private ArrayList<Integer[]> getParkingSpot(Integer size) {
+        ArrayList<Integer[]> parkingSpots = new ArrayList<>();
+        for (int i = 0; i < levels; i++) {
+            for (int j = 0; j < height; j++) {
+                for (int k = 0; k < width; k++) {
+                    if (size == 1) {
+                        for (int l = 0; l < 2; l++) {
+                            if (map[i][j][k][l] == null) {
+                                parkingSpots.add(new Integer[]{i, j, k, l});
+                                return parkingSpots;
+                            }
+                        }
+                    } else if (size == 2) {
+                        if (map[i][j][k][0] == null && map[i][j][k][1] == null) {
+                            parkingSpots.add(new Integer[]{i, j, k, 0});
+                            parkingSpots.add(new Integer[]{i, j, k, 1});
+                            return parkingSpots;
+                        }
+                    } else {
+                        if (map[i][j][k][0] == null && map[i][j][k][1] == null) {
+                            if (j < height - 1 && map[i][j + 1][k][0] == null && map[i][j + 1][k][1] == null) {
+                                parkingSpots.add(new Integer[]{i, j, k, 0});
+                                parkingSpots.add(new Integer[]{i, j, k, 1});
+                                parkingSpots.add(new Integer[]{i + 1, j, k, 0});
+                                parkingSpots.add(new Integer[]{i + 1, j, k, 1});
+                                return parkingSpots;
+                            } else if (k < width - 1 && map[i][j][k + 1][0] == null && map[i][j][k + 1][1] == null) {
+                                parkingSpots.add(new Integer[]{i, j, k, 0});
+                                parkingSpots.add(new Integer[]{i, j, k, 1});
+                                parkingSpots.add(new Integer[]{i, j + 1, k, 0});
+                                parkingSpots.add(new Integer[]{i, j + 1, k, 1});
+                                return parkingSpots;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return parkingSpots;
     }
 
     /**
@@ -52,7 +119,28 @@ public class MultiLevelParkingLot extends ParkingLot {
      */
     @Override
     public String getTable() {
-        return "";
+        LinkedList<String> tableRows = new LinkedList<>();
+        for (int k = 0; k < levels; k++) {
+            Car[][][] parkingLevel = map[k];
+            for (Car[][] parkingSpotRow : parkingLevel) {
+                String[] addedRows = {"", ""};
+                for (Car[] parkingSpot : parkingSpotRow) {
+                    for (int i = 0; i < 2; i++) {
+                        if (parkingSpot[i] == null) {
+                            addedRows[i] = addedRows[i].concat("..");
+                        } else {
+                            addedRows[i] = addedRows[i].concat(parkingSpot[i].toString());
+                        }
+                    }
+                }
+                tableRows.add(addedRows[0]);
+                tableRows.add(addedRows[1]);
+            }
+            if (k != levels - 1) {
+                tableRows.add("---");
+            }
+        }
+        return String.join("\n", tableRows);
     }
 
     @Override
@@ -62,6 +150,16 @@ public class MultiLevelParkingLot extends ParkingLot {
 
     @Override
     public void unparkCar(Car car) {
-
+        if (!car.isParked()) {
+            queue.remove(car);
+        } else {
+            ArrayList<Integer[]> coords = locations.get(car);
+            locations.remove(car);
+            for (Integer[] coord : coords) {
+                map[coord[0]][coord[1]][coord[2]][coord[3]] = null;
+            }
+            parkedCars.remove(car);
+        }
+        processQueue();
     }
 }
