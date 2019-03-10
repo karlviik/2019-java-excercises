db31f4264ba58af9d7ef875f782016e2d88345b1
karlviik <viik.karl2@gmail.com>
2019-03-10 16:55:36 +0200

    PriorityParkingLot Initial

--- EX05ParkingLot/src/ee/taltech/iti0202/parking/car/Car.java
+++ EX05ParkingLot/src/ee/taltech/iti0202/parking/car/Car.java
@@ -69,6 +69,10 @@ public class Car implements Comparable<Car> {
         isParked = true;
     }
 
+    public void setUnparked() {
+        isParked = false;
+    }
+
     public ParkingLot getLocation() {
         return location;
     }
--- EX05ParkingLot/src/ee/taltech/iti0202/parking/parkinglot/PriorityParkingLot.java
+++ EX05ParkingLot/src/ee/taltech/iti0202/parking/parkinglot/PriorityParkingLot.java
@@ -2,6 +2,10 @@ package ee.taltech.iti0202.parking.parkinglot;
 
 import ee.taltech.iti0202.parking.car.Car;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Accepts all cars if the queue size is less than 5.
  * Small car (size 1) with the highest priority can park alone.
@@ -11,6 +15,13 @@ import ee.taltech.iti0202.parking.car.Car;
  *
  */
 public class PriorityParkingLot extends ParkingLot {
+
+    private Car[][][] map;
+    private HashMap<Car, ArrayList<Integer[]>> locations = new HashMap<>();
+    private ArrayList<Integer[]> emptySlots = new ArrayList<>();
+    // queue
+    // parkedcars
+
     /**
      * Initialize the parking slot with the given width and height.
      *
@@ -19,16 +30,121 @@ public class PriorityParkingLot extends ParkingLot {
      */
     public PriorityParkingLot(int height, int width) {
         super(height, width);
+        map = new Car[height][width][2];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                emptySlots.add(new Integer[] {i, j, 0});
+                emptySlots.add(new Integer[] {i, j, 1});
+            }
+        }
     }
 
     @Override
     public void processQueue() {
+        while (queue.size() > 0) {
+            Car car = queue.peek();
+            ArrayList<Integer[]> coords = getParkingSpot(car.getSize(), car.getPriorityStatus());
+            if (coords.size() == 0) {
+                if (car.getPriorityStatus() == Car.PriorityStatus.HIGHEST) {
+                    return;
+                } else {
+                    if (unparkAndQueueAllCommonCars()) {
+                        continue;
+                    }
+                    return;
+                }
+            }
+            parkedCars.add(queue.poll());
+            locations.put(car, coords);
+            for (Integer[] coord : coords) {
+                emptySlots.removeIf(x -> Arrays.equals(x, coord));
+                map[coord[0]][coord[1]][coord[2]] = car;
+            }
 
+        }
+    }
+
+    private ArrayList<Integer[]> getParkingSpot(Integer size, Car.PriorityStatus status) {
+        ArrayList<Integer[]> parkingSpots = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (size == 1 && status != Car.PriorityStatus.HIGHEST) {
+                    for (int k = 0; k < 2; k++) {
+                        if (map[i][j][k] == null) {
+                            if (map[i][j][(k + 1) % 2] != null) {
+                                if (map[i][j][(k + 1) % 2].getPriorityStatus() == status) {
+                                    parkingSpots.add(new Integer[]{i, j, k});
+                                    return parkingSpots;
+                                }
+                            } else {
+                                parkingSpots.add(new Integer[]{i, j, k});
+                                return parkingSpots;
+                            }
+                        }
+                    }
+                } else if (size == 2 || size == 1) {
+                    if (map[i][j][0] == null && map[i][j][1] == null) {
+                        parkingSpots.add(new Integer[] {i, j, 0});
+                        if (size != 1) {
+                            parkingSpots.add(new Integer[] {i, j, 1});
+                        }
+                        return parkingSpots;
+                    }
+                } else {
+                    if (map[i][j][0] == null && map[i][j][1] == null) {
+                        if ( i < height - 1 && map[i + 1][j][0] == null && map[i + 1][j][1] == null) {
+                            parkingSpots.add(new Integer[] {i, j, 0});
+                            parkingSpots.add(new Integer[] {i, j, 1});
+                            parkingSpots.add(new Integer[] {i + 1, j, 0});
+                            parkingSpots.add(new Integer[] {i + 1, j, 1});
+                            return parkingSpots;
+                        } else if (j < width - 1 && map[i][j + 1][0] == null && map[i][j + 1][1] == null) {
+                            parkingSpots.add(new Integer[] {i, j, 0});
+                            parkingSpots.add(new Integer[] {i, j, 1});
+                            parkingSpots.add(new Integer[] {i, j + 1, 0});
+                            parkingSpots.add(new Integer[] {i, j + 1, 1});
+                            return parkingSpots;
+                        }
+                    }
+                }
+            }
+        }
+        return parkingSpots;
+    }
+
+    private boolean unparkAndQueueAllCommonCars() {
+        List<Car> parkedCommonCars = parkedCars.stream()
+                .filter(x -> x.getPriorityStatus() == Car.PriorityStatus.COMMON)
+                .collect(Collectors.toList());
+        if (parkedCommonCars.size() == 0) {
+            return false;
+        }
+        for (Car car : parkedCommonCars) {
+            removeFromLists(car);
+            queue.add(car);
+            car.setUnparked();
+        }
+        return true;
     }
 
     @Override
     public String getTable() {
-        return null;
+        LinkedList<String> tableRows = new LinkedList<>();
+        for (Car[][] parkingSpotRow : map) {
+            String[] addedRows = {"", ""};
+            for (Car[] parkingSpot : parkingSpotRow) {
+                for (int i = 0; i < 2; i++) {
+                    if (parkingSpot[i] == null) {
+                        addedRows[i] = addedRows[i].concat("..");
+                    } else {
+                        addedRows[i] = addedRows[i].concat(parkingSpot[i].toString());
+                    }
+                }
+            }
+            tableRows.add(addedRows[0]);
+            tableRows.add(addedRows[1]);
+        }
+        return String.join("\n", tableRows);
     }
 
     @Override
@@ -38,6 +154,18 @@ public class PriorityParkingLot extends ParkingLot {
 
     @Override
     public void unparkCar(Car car) {
+        removeFromLists(car);
+        processQueue();
+    }
 
+    private void removeFromLists(Car car) {
+        if (!car.isParked()) {
+            queue.remove(car);
+        } else {
+            ArrayList<Integer[]> coords = locations.get(car);
+            locations.remove(car);
+            emptySlots.addAll(coords);
+            parkedCars.remove(car);
+        }
     }
 }
