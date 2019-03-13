74f862969290e6abffd6bce21f57984b22cecac8
karlviik <viik.karl2@gmail.com>
2019-03-13 15:40:13 +0200

    Remove unused import

--- PR07Logger/src/ee/taltech/iti0202/logger/FileLogger.java
+++ PR07Logger/src/ee/taltech/iti0202/logger/FileLogger.java
@@ -4,7 +4,6 @@ import ee.taltech.iti0202.logger.formatter.LogFormatter;
 import ee.taltech.iti0202.logger.level.Level;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
