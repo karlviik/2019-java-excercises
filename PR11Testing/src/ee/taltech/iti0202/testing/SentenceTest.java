42466c3de9934cf96863e8b6547b602814eb271e
karlviik <viik.karl2@gmail.com>
2019-04-15 00:33:09 +0300

    SentenceTestTest

--- PR11Testing/src/ee/taltech/iti0202/testing/SentenceTest.java
+++ PR11Testing/src/ee/taltech/iti0202/testing/SentenceTest.java
@@ -485,4 +485,25 @@ public class SentenceTest {
         assertTrue(actual);
     }
 
+    @Test
+    public void testEquals_EqualTwoSameSentencesPunctuationDifference6_IsTrue() {
+        Sentence sentence1 = new Sentence("Heya...");
+        Sentence sentence2 = new Sentence("Heya");
+
+        boolean actual = sentence1.equals(sentence2);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testEquals_EqualTwoSameSentencesPunctuationDifference7_IsTrue() {
+        Sentence sentence1 = new Sentence("Heya");
+        Sentence sentence2 = new Sentence("Heya");
+
+        sentence2.addPunctuation("...");
+        boolean actual = sentence1.equals(sentence2);
+
+        assertTrue(actual);
+    }
+
 }
