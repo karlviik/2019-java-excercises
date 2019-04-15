7ef92660a80270881facd831a65f5c2dcf5c3879
karlviik <viik.karl2@gmail.com>
2019-04-15 03:27:56 +0300

    Test

--- EX11Birdwatching/src/ee/taltech/iti0202/birdwatching/bird/BirdDataController.java
+++ EX11Birdwatching/src/ee/taltech/iti0202/birdwatching/bird/BirdDataController.java
@@ -1,36 +1,29 @@
 package ee.taltech.iti0202.birdwatching.bird;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class BirdDataController {
     private List<Bird> birds = new ArrayList<>();
 
     public void readBirdDataFromCsvFile(String filename) throws BirdDataException {
         Path path = Paths.get(filename);
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            BirdBuilder bb = new BirdBuilder();
-            while (true) {
-                String line = reader.readLine();
-                if (line == null) break;
-
-                List<String> fields = Arrays.asList(line.split(","));
-                birds.add(
-                        bb.setSpecies(fields.get(0))
-                        .setWeight(Double.valueOf(fields.get(1)))
-                        .setWingspan(Double.valueOf(fields.get(2)))
-                        .setAge(Bird.Age.valueOf(fields.get(3).toUpperCase()))
-                        .setSex(Bird.Sex.valueOf(fields.get(4).toUpperCase()))
-                        .createBird()
-                );
-
-            }
+        BirdBuilder bb = new BirdBuilder();
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.map(x -> x.split(","))
+            .forEach(fields -> birds.add(
+                bb.setSpecies(fields[0])
+                    .setWeight(Double.valueOf(fields[1]))
+                    .setWingspan(Double.valueOf(fields[2]))
+                    .setAge(Bird.Age.valueOf(fields[3].toUpperCase()))
+                    .setSex(Bird.Sex.valueOf(fields[4].toUpperCase()))
+                    .createBird()
+            ));
         } catch (IOException e) {
             throw new BirdDataException(e);
         }
