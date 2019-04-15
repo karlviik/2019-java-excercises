35c33b66af6ddf9a0456be1a9c775927082638b1
karlviik <viik.karl2@gmail.com>
2019-04-15 10:44:52 +0300

    Test

--- EX11Birdwatching/src/ee/taltech/iti0202/birdwatching/statistics/StatisticsCalculator.java
+++ EX11Birdwatching/src/ee/taltech/iti0202/birdwatching/statistics/StatisticsCalculator.java
@@ -38,7 +38,7 @@ public class StatisticsCalculator {
     }
 
     public long countBirds() {
-        // could use .stream().count(); , but that's excessive
+        // could use .stream().count(); , but that's excessive.
         return getBirds().size();
     }
 
