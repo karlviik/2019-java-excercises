c254d47116a297985aba6852071328f0b4259cb7
karlviik <viik.karl2@gmail.com>
2019-05-07 23:07:46 +0300

    Test

--- EX13Weather/src/ee/taltech/iti0202/api/strategies/LovesChangeCityFinder.java
+++ EX13Weather/src/ee/taltech/iti0202/api/strategies/LovesChangeCityFinder.java
@@ -2,11 +2,12 @@ package ee.taltech.iti0202.api.strategies;
 
 import ee.taltech.iti0202.api.destinations.City;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class LovesChangeCityFinder implements CityFinderStrategy {
-  // TODO: this
+// TODO: this
 
   /**
    * Klient soovib, et ilm oleks võimalikult erinev iga päev, arvestada tuleb temperatuuri, niiskuse ning kliima muutust. Temperatuuri ning niiskuse kõikumist arvestage lihtsalt iga päeva keskmise väärtuse kõikumisega. Ilma koodide kõikumisel tuleb arvestada kolme asjaga:
@@ -18,6 +19,54 @@ public class LovesChangeCityFinder implements CityFinderStrategy {
    */
   @Override
   public Optional<City> findBestCity(List<City> candidateCities) {
-    return Optional.empty();
+    Double bestWeight = Double.MIN_VALUE;
+    Optional<City> bestCity = Optional.empty();
+    for (City city : candidateCities) {
+      Double weight = 0d;
+      int miniCounter = 0;
+      Double tempSum = 0d;
+      Double humidSum = 0d;
+      List<Double> tempDiffs = new ArrayList<>();
+      List<Double> humidDiffs = new ArrayList<>();
+      List<Double> temps = city.getTemperatures();
+      List<Double> humids = city.getHumidity();
+      List<Integer> codes = city.getWeatherCodes();
+      Integer lastCode = 0;
+      Integer code;
+      for (int i = 0; i < 40; i++) {
+        tempSum += temps.get(i);
+        humidSum += humids.get(i);
+        miniCounter++;
+        if (miniCounter == 8) {
+          miniCounter = 0;
+          tempDiffs.add(tempSum);
+          humidDiffs.add(humidSum);
+          humidSum = 0d;
+          tempSum = 0d;
+        }
+        code = codes.get(i);
+        if (i > 0) {
+          if (code.equals(lastCode)) {
+            weight -= 10;
+          }
+          else if (code / 100 != lastCode / 100) {
+            weight += 100;
+          }
+          else {
+            weight += 20;
+          }
+        }
+        lastCode = code;
+      }
+      for (int i = 1; i < 5; i++) {
+        weight += Math.abs(tempDiffs.get(i) - tempDiffs.get(i - 1));
+        weight += Math.abs(humidDiffs.get(i) - humidDiffs.get(i - 1));
+      }
+      if (bestWeight < weight) {
+        bestWeight = weight;
+        bestCity = Optional.of(city);
+      }
+    }
+    return bestCity;
   }
 }
