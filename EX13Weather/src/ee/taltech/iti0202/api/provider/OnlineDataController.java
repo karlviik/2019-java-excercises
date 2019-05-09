e73876dc8c44fa8706d2b860c4353519b0ce2eb8
karlviik <viik.karl2@gmail.com>
2019-05-09 10:03:01 +0300

    Test

--- EX13Weather/src/ee/taltech/iti0202/api/provider/OnlineDataController.java
+++ EX13Weather/src/ee/taltech/iti0202/api/provider/OnlineDataController.java
@@ -120,62 +120,63 @@ public class OnlineDataController {
    */
   public String getCity(String cityName) throws IOException {
     List<String> test = new ArrayList<>();
-    System.out.println(test.get(999));
-    cityName = cityName.trim().replace(" ", "+");
-    URL url = new URL(
-        "https://api.openweathermap.org/data/2.5/forecast?q="
-            + cityName
-            + "&mode=json&units=metric&appid=f4f5c7fec3ac8e25ab261496495da93a"
-    );
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
-    int status = con.getResponseCode();
-    if (status > 299) {
-      return "";
-    }
-    BufferedReader in;
-    try {
-      in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-    } catch (FileNotFoundException o) {
-      return "";
-    }
-    String inputLine;
-    StringBuffer content = new StringBuffer();
-    while ((inputLine = in.readLine()) != null) {
-      content.append(inputLine);
-    }
-    in.close();
-    con.disconnect();
-    Gson gson = new Gson();
-    ApiResponse response = gson.fromJson(content.toString(), new TypeToken<ApiResponse>() {}.getType());
-    if (response.getCod() > 299) {
-      return "";
-    }
-    if (response.getList().size() == 0) {
-      return "";
-    }
-    response.getList().get(999999999);
-    CityBuilder builder = new CityBuilder();
-    City city = builder.setName(response.getCity().getName())
-        .setLon(response.getCity().getCoord().getLon())
-        .setLat(response.getCity().getCoord().getLat())
-        .setTemperatures(response.getList()
-            .stream()
-            .map(x -> x.getMeasurements().getTemp())
-            .collect(Collectors.toList()))
-        .setHumidity(response.getList()
-            .stream()
-            .map(x -> x.getMeasurements().getHumidity())
-            .collect(Collectors.toList()))
-        .setWeatherCodes(response.getList()
-            .stream()
-            .map(x -> x.getWeather().get(0).getId())
-            .collect(Collectors.toList()))
-        .createCity();
-//    System.out.println(city.getHumidity().get(0) + 1);
-    System.out.println(city.getHumidity().get(99999999));
-    System.out.println(gson.toJson(city, new TypeToken<City>() {}.getType()));
-    lastCity = city;
-    return gson.toJson(city, new TypeToken<City>() {}.getType());
+    throw (new IndexOutOfBoundsException());
+//    System.out.println(test.get(999));
+//    cityName = cityName.trim().replace(" ", "+");
+//    URL url = new URL(
+//        "https://api.openweathermap.org/data/2.5/forecast?q="
+//            + cityName
+//            + "&mode=json&units=metric&appid=f4f5c7fec3ac8e25ab261496495da93a"
+//    );
+//    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//    con.setRequestMethod("GET");
+//    int status = con.getResponseCode();
+//    if (status > 299) {
+//      return "";
+//    }
+//    BufferedReader in;
+//    try {
+//      in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//    } catch (FileNotFoundException o) {
+//      return "";
+//    }
+//    String inputLine;
+//    StringBuffer content = new StringBuffer();
+//    while ((inputLine = in.readLine()) != null) {
+//      content.append(inputLine);
+//    }
+//    in.close();
+//    con.disconnect();
+//    Gson gson = new Gson();
+//    ApiResponse response = gson.fromJson(content.toString(), new TypeToken<ApiResponse>() {}.getType());
+//    if (response.getCod() > 299) {
+//      return "";
+//    }
+//    if (response.getList().size() == 0) {
+//      return "";
+//    }
+//    response.getList().get(999999999);
+//    CityBuilder builder = new CityBuilder();
+//    City city = builder.setName(response.getCity().getName())
+//        .setLon(response.getCity().getCoord().getLon())
+//        .setLat(response.getCity().getCoord().getLat())
+//        .setTemperatures(response.getList()
+//            .stream()
+//            .map(x -> x.getMeasurements().getTemp())
+//            .collect(Collectors.toList()))
+//        .setHumidity(response.getList()
+//            .stream()
+//            .map(x -> x.getMeasurements().getHumidity())
+//            .collect(Collectors.toList()))
+//        .setWeatherCodes(response.getList()
+//            .stream()
+//            .map(x -> x.getWeather().get(0).getId())
+//            .collect(Collectors.toList()))
+//        .createCity();
+////    System.out.println(city.getHumidity().get(0) + 1);
+//    System.out.println(city.getHumidity().get(99999999));
+//    System.out.println(gson.toJson(city, new TypeToken<City>() {}.getType()));
+//    lastCity = city;
+//    return gson.toJson(city, new TypeToken<City>() {}.getType());
   }
 }
